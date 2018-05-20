9f14782e649b4b4feaf56f1dfdde84c1dfd16419
GuyTevet <guy.tvt@gmail.com>
2018-05-20 20:12:20 +0300

    data utils

---
+++ src/data.py
@@ -0,0 +1,68 @@
+##header here
+
+import os
+
+#consts
+ASCII_OFFSET = 32
+ASCII_LEN = 126 - 32
+UNK_TAG = ASCII_LEN # '~'
+START_TAG = UNK_TAG + 1
+END_TAG = UNK_TAG + 2
+
+def char2tag(char):
+    assert isinstance(char, basestring)
+    assert len(char) == 1
+    tag = ord(char) - ASCII_OFFSET
+    if tag < 0 or tag > (ASCII_LEN - 1):
+        #print char #for debug UNKS
+        tag = UNK_TAG
+    return tag
+
+def tag2char(tag):
+    assert type(tag) == int
+    return chr(tag + ASCII_OFFSET)
+
+def text2sents(text):
+    return text.split('\n')
+
+def text2feed_tags(text,max_len=32):
+    sents = text2sents(text)
+    feed_tags = []
+
+    for sent in sents:
+        tags = [START_TAG] + ([END_TAG] * (max_len + 1))
+        for i, char in enumerate(sent[:max_len]):
+            tags[i+1] = char2tag(char)
+        feed_tags.append(tags)
+
+    return feed_tags
+
+def test():
+
+    sanity_text_path = os.path.join('..','data','europarl-v6.en')
+
+    for char in 'hello world':
+        tag = char2tag(char)
+        print str(tag) + '\t' + tag2char(tag)
+
+    chars = [tag2char(i) for i in range(ASCII_LEN + 1)]
+    hist = [0] * (ASCII_LEN + 1)
+
+
+    with open(sanity_text_path,'r') as file:
+        text = file.read()
+        sents = text2sents(text)
+        print 'sanity text len: ' + str(len(text))
+
+    for sent in sents[:1000]:
+        for char in sent:
+            hist[char2tag(char)] += 1
+
+    for i in range(len(chars)):
+        print chars[i] + '\t' + str(hist[i])
+
+    feed_tags = text2feed_tags(text[:100000],max_len=64)
+
+
+if __name__ == '__main__':
+    test()
