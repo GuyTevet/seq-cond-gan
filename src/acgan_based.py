66799349a7613c6175d2cbb424b7ff73d1362714
Gavriel Habib <gavri1991@gmail.com>
2018-06-01 10:58:51 +0300

    Merge branch 'master' of https://github.com/GuyTevet/seq-cond-gan
    # Conflicts:
    #	src/acgan_based.py

--- src/acgan_based.py
+++ src/acgan_based.py
@@ -13,8 +13,8 @@ import data
 from ops import *
 from utils import *
 
-class baseline(object):
-    model_name = "baseline"     # name for checkpoint
+class acgan_based(object):
+    model_name = "acgan_based"     # name for checkpoint
 
     def __init__(self, sess, epoch, batch_size, z_dim, dataset_name, checkpoint_dir, result_dir, log_dir):
         self.sess = sess
@@ -28,6 +28,8 @@ class baseline(object):
         if dataset_name == 'sanity_data':
             # parameters
             self.embed_size = 512
+            self.class_embed_size = 32
+            self.class_num = 3
             self.hidden_size = self.embed_size
             self.dropout_rate = 0.5
             self.z_dim = self.hidden_size
@@ -83,7 +85,7 @@ class baseline(object):
 
             return out, out_logit, out_for_classifier
 
-    def generator(self, z, data_input, mask, max_len, is_training=True, reuse=False):
+    def generator(self, z, data_input, mask, max_len, class_input, is_training=True, reuse=False):
         # Network Architecture is exactly same as in infoGAN (https://arxiv.org/abs/1606.03657)
         # Architecture : FC1024_BR-FC7x7x128_BR-(64)4dc2s_BR-(1)4dc2s_S
         with tf.variable_scope("generator", reuse=reuse):
@@ -93,9 +95,17 @@ class baseline(object):
             #define embedding matrix
             g_embeddings = tf.get_variable(name='g_embeddings', shape=[data.TAG_NUM, self.embed_size], dtype=tf.float32,
                                          initializer=tf.contrib.layers.xavier_initializer()) #dims=[tag_num,embed_size]
+            #define class embeddings
+            g_class_embed_matrix = tf.get_variable(name='g_class_embed_matrix', shape=[self.class_num, self.class_embed_size],
+                                                 dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer()) #dims=[class_num,class_embed_size]
+
+            # extract class embeddings
+            g_class_embed = tf.nn.embedding_lookup(g_class_embed_matrix, class_input, name='g_class_embed')#dims=[bs,class_embed_size]
+            g_class_embed_tiled = tf.tile(tf.expand_dims(g_class_embed, axis=1), [1, self.seq_len+2, 1], name='g_class_embed_tiled') #dims=[bs,max_len+2,class_embed_size]
 
             #get embeddings of the real data
             data_embeddings = tf.nn.embedding_lookup(g_embeddings, data_input, name='data_embeddings') #dims=[bs,max_len+2,embed_size]
+            data_embeddings = tf.concat([data_embeddings, g_class_embed_tiled], axis=2, name='data_embeddings_concat') #dims=[bs,max_len+2,embed_size+class_embed_size]
 
             #get one hot vectors of the real data
             data_one_hot = tf.one_hot(data_input, data.TAG_NUM, axis=2, dtype=tf.float32, name='data_one_hot') #dims=[bs,max_len+2,tag_num]
@@ -117,11 +127,14 @@ class baseline(object):
                     output_embeddings = tf.matmul(output_prob[:, i-1, :], g_embeddings, name='output_embeddings') #dims=[bs,embed_size]
                 else:
                     output_embeddings = tf.zeros([self.batch_size, self.embed_size])
+
+                # concatanate class embeddings to output_embeddings
+                output_embeddings = tf.concat([output_embeddings, g_class_embed], axis=1, name='output_embeddings_concat')  # dims=[bs,embed_size+class_embed_size]
                 x_data = tf.multiply(tf.tile(tf.expand_dims(tf.cast(1-mask[:, i], dtype=tf.float32),axis=1),
-                                             [1,self.embed_size]), data_embeddings[:, i, :]) #dims=[bs,embed_size]
+                                             [1,self.embed_size+self.class_embed_size]), data_embeddings[:, i, :]) #dims=[bs,embed_size+class_embed_size]
                 x_gen = tf.multiply(tf.tile(tf.expand_dims(tf.cast(mask[:, i], dtype=tf.float32),axis=1),
-                                            [1, self.embed_size]), output_embeddings) #dims=[bs,embed_size]
-                x = x_data + x_gen #dims=[bs,embed_size]
+                                            [1, self.embed_size+self.class_embed_size]), output_embeddings) #dims=[bs,embed_size+class_embed_size]
+                x = x_data + x_gen #dims=[bs,embed_size+class_embed_size]
 
                 g_probs = tf.cond(pred=tf.less(tf.constant(i), max_len + 2),
                                  true_fn=lambda: tf.nn.softmax(linear(tf.nn.dropout((gru(x, h, scope='g_gru'))[1], self.dropout_rate, name='o_drop_t'), data.TAG_NUM, scope='g_fc'),axis=1),
@@ -145,29 +158,40 @@ class baseline(object):
 
             return out, debug_dict
 
-    def build_model(self):
-        # some parameters
-        # image_dims = [self.input_height, self.input_width, self.c_dim]
-        # bs = self.batch_size
+    def classifier(self, x, is_training=True, reuse=False):
+        with tf.variable_scope('classifier', reuse=reuse):
+            net = lrelu(bn(linear(x, self.hidden_size, scope='c_fc1'), is_training=is_training, scope='c_bn1'))
+            out_logit = linear(net, self.class_num, scope='c_fc2')
+            out = tf.nn.softmax(out_logit)
 
+        return out, out_logit
+
+    def build_model(self):
         """ Graph Input """
         # generator inputs
         self.generator_input = tf.placeholder(shape=[self.batch_size, self.seq_len+2], dtype=tf.int32, name='generator_input')
         self.generator_mask = tf.placeholder(shape=[self.batch_size, self.seq_len+2], dtype=tf.int32, name='generator_mask')
         self.z = tf.placeholder(tf.float32, shape=[self.batch_size, self.hidden_size], name='z')
         self.max_len = tf.placeholder(tf.int32, name='max_len')
+        self.generator_class = tf.placeholder(dtype=tf.int32, name='generator_class', shape=self.batch_size)
+        self.generator_class_one_hot = tf.one_hot(self.generator_class, dtype=tf.float32, depth=self.class_num)
+
 
         # discrimnator input
         self.discrimnator_input = tf.placeholder(shape=[self.batch_size, self.seq_len+2], dtype=tf.int32, name='discrimnator_input')
         self.discrimnator_input_one_hot = tf.one_hot(self.discrimnator_input, dtype=tf.float32, depth=data.TAG_NUM)
+        self.discriminator_class = tf.placeholder(dtype=tf.int32, name='discriminator_class', shape=self.batch_size)
+        self.discriminator_class_one_hot = tf.one_hot(self.discriminator_class, dtype=tf.float32, depth=self.class_num)
+
 
         """ Loss Function """
         # output of D for real images
-        D_real, D_real_logits, _ = self.discriminator(self.discrimnator_input_one_hot,self.max_len, is_training=True, reuse=False)
+        D_real, D_real_logits, input4classifier_real = self.discriminator(self.discrimnator_input_one_hot,self.max_len, is_training=True, reuse=False)
 
         # output of D for fake images
-        G, self.g_debug_dict = self.generator(self.z, self.generator_input, self.generator_mask, self.max_len, is_training=True, reuse=False)
-        D_fake, D_fake_logits, _ = self.discriminator(G, self.max_len, is_training=True, reuse=True)
+        G, self.g_debug_dict = self.generator(self.z, self.generator_input, self.generator_mask, self.max_len,
+                                              class_input=self.generator_class, is_training=True, reuse=False)
+        D_fake, D_fake_logits, input4classifier_fake = self.discriminator(G, self.max_len, is_training=True, reuse=True)
 
         # get loss for discriminator
         d_loss_real = - tf.reduce_mean(D_real_logits)
@@ -177,6 +201,21 @@ class baseline(object):
 
         # get loss for generator
         self.g_loss = - d_loss_fake
+
+        # get classifier loss
+        code_real, code_logit_real = self.classifier(input4classifier_real, is_training=True, reuse=False) #dims=[bs, class_num]
+        code_fake, code_logit_fake = self.classifier(input4classifier_fake, is_training=True, reuse=True) #dims=[bs, class_num]
+
+        # For real samples
+        q_real_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=code_logit_real,
+                                                                             labels=self.discriminator_class_one_hot))
+
+        # For fake samples
+        q_fake_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=code_logit_fake,
+                                                                             labels=self.generator_class_one_hot))
+
+        # get classifier loss
+        self.q_loss = q_fake_loss + q_real_loss
 
         """ Gradient Penalty """
         # This is borrowed from https://github.com/kodalinaveen3/DRAGAN/blob/master/DRAGAN.ipynb
@@ -194,6 +233,8 @@ class baseline(object):
         t_vars = tf.trainable_variables()
         d_vars = [var for var in t_vars if 'd_' in var.name]
         g_vars = [var for var in t_vars if 'g_' in var.name]
+        q_vars = [var for var in t_vars if ('d_' in var.name) or ('c_' in var.name) or ('g_' in var.name)] #FIXME - why do we need g_vars?
+        c_vars = [var for var in t_vars if 'c_' in var.name]
 
         # optimizers
         with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
@@ -201,26 +242,40 @@ class baseline(object):
                       .minimize(self.d_loss, var_list=d_vars)
             self.g_optim = tf.train.AdamOptimizer(self.learning_rate*5, beta1=self.beta1) \
                       .minimize(self.g_loss, var_list=g_vars)
+            self.q_optim = tf.train.AdamOptimizer(self.learning_rate*5, beta1=self.beta1) \
+                      .minimize(self.q_loss, var_list=q_vars)
 
         """" Testing """
         # for test
-        self.fake_sents, _ = self.generator(self.z, self.generator_input, self.generator_mask, max_len=self.max_len, is_training=False, reuse=True)
+        self.fake_sents, _ = self.generator(self.z, self.generator_input, self.generator_mask, max_len=self.max_len,
+                                            class_input=self.generator_class, is_training=False, reuse=True)
 
         """ Summary """
         d_loss_real_sum = tf.summary.scalar("d_loss_real", d_loss_real)
         d_loss_fake_sum = tf.summary.scalar("d_loss_fake", d_loss_fake)
         d_loss_sum = tf.summary.scalar("d_loss", self.d_loss)
+
+        q_loss_real_sum = tf.summary.scalar("q_loss_real_sum", q_real_loss)
+        q_loss_fake_sum = tf.summary.scalar("q_loss_fake_sum", q_fake_loss)
+        q_loss_sum = tf.summary.scalar("q_loss_sum", self.q_loss)
+
         g_loss_sum = tf.summary.scalar("g_loss", self.g_loss)
+
         g_vars_sum = [tf.summary.histogram(var.name, var) for var in g_vars]
         d_vars_sum = [tf.summary.histogram(var.name, var) for var in d_vars]
-        g_output_sum = tf.summary.histogram('G_output',G)
+        c_vars_sum = [tf.summary.histogram(var.name, var) for var in c_vars]
+
+        g_output_sum = tf.summary.histogram('G_output', G)
         d_output_fake_sum = tf.summary.histogram('D_fake', D_fake)
         d_output_real_sum = tf.summary.histogram('D_real', D_real)
+        q_output_fake_sum = tf.summary.histogram('Q_fake', code_fake)
+        q_output_real_sum = tf.summary.histogram('Q_real', code_real)
 
         # final summary operations
-        self.g_sum = tf.summary.merge([d_loss_fake_sum, g_loss_sum , g_output_sum] + g_vars_sum)
+        self.g_sum = tf.summary.merge([d_loss_fake_sum, g_loss_sum, g_output_sum] + g_vars_sum)
         self.d_sum = tf.summary.merge([d_loss_real_sum, d_loss_sum, d_output_fake_sum, d_output_real_sum] + d_vars_sum)
-
+        self.q_sum = tf.summary.merge([q_loss_sum, q_loss_real_sum, q_loss_fake_sum, q_output_real_sum,
+                                       q_output_fake_sum] + c_vars_sum)
 
     def train(self):
 
@@ -276,23 +331,27 @@ class baseline(object):
                 batch_mask_generator = batch_mask[:self.batch_size]
 
                 # update D network
-                _, summary_str, d_loss = self.sess.run([self.d_optim, self.d_sum, self.d_loss],
+                _, summary_str, d_loss, q_loss = self.sess.run([self.d_optim, self.d_sum, self.d_loss, self.q_loss],
                                                feed_dict={self.discrimnator_input: batch_sents_discriminator,
                                                           self.generator_input: batch_sents_generator,
                                                           self.generator_mask: batch_mask_generator,
+                                                          self.generator_class: np.ones(self.batch_size, dtype=np.int32),
+                                                          self.discriminator_class: np.ones(self.batch_size, dtype=np.int32),
                                                           self.z: batch_z,
-                                                          self.max_len: cur_max_len})
+                                                          self.max_len: cur_max_len}) #FIXME - update generator and discriminator class
                 self.writer.add_summary(summary_str, counter)
 
                 # update G network
                 if (counter-1) % self.disc_iters == 0:
                     batch_z = np.random.normal(0, 1, [self.batch_size, self.z_dim]).astype(np.float32)
-                    _, summary_str, g_loss = self.sess.run([self.g_optim, self.g_sum, self.g_loss],
+                    _, summary_str, c_summary_str, g_loss = self.sess.run([self.g_optim, self.g_sum, self.q_sum, self.g_loss],
                                                            feed_dict={self.z: batch_z,
                                                                       self.generator_mask: batch_mask_generator,
                                                                       self.generator_input: batch_sents_generator,
-                                                                      self.max_len: cur_max_len})
+                                                                      self.generator_class: np.ones(self.batch_size, dtype=np.int32),
+                                                                      self.max_len: cur_max_len}) #FIXME - update generator class
                     self.writer.add_summary(summary_str, counter)
+                    self.writer.add_summary(c_summary_str, counter)
 
                 # #for debug
                 # if idx % 10 == 0:
@@ -312,8 +371,8 @@ class baseline(object):
 
 
                 # display training status
-                print("Epoch: [%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f" \
-                      % (epoch, idx, self.num_batches, time.time() - start_time, d_loss, g_loss))
+                print("Epoch: [%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f, q_loss: %.8f" \
+                      % (epoch, idx, self.num_batches, time.time() - start_time, d_loss, g_loss, q_loss))
 
                 # # save training results for every 300 steps
                 # if np.mod(counter, 300) == 0:
@@ -350,7 +409,8 @@ class baseline(object):
         samples = self.sess.run(self.fake_sents, feed_dict={self.z: z_sample,
                                                             self.generator_mask: batch_empty_mask,
                                                             self.generator_input: batch_empty_data,
-                                                            self.max_len: max_len})
+                                                            self.generator_class: np.ones(self.batch_size, dtype=np.int32),
+                                                            self.max_len: max_len}) #FIXME - update generator class
         tags = np.argmax(samples, axis=2).astype(int)
         sentences = []
 
